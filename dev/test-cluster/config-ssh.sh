7b6aafcc8b087eea1a55b1f2f11cd48436f0bd95
Wu, Xiaochang <xiaochang.wu@intel.com>
2021-03-31 21:44:13 +0800

    fix config-ssh

--- dev/test-cluster/config-ssh.sh
+++ dev/test-cluster/config-ssh.sh
@@ -7,8 +7,9 @@ chmod 600 ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 echo "    StrictHostKeyChecking no                     " | sudo tee -a /etc/ssh/ssh_config
+echo "    PasswordAuthentication no                    " | sudo tee -a /etc/ssh/ssh_config
 
 ls -l ~/.ssh
 sudo service ssh restart
 
-ssh localhost
+ssh -vvv localhost
