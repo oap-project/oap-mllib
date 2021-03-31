1eb3eb3c5445ac8e3c231b85c8acf1ac74ec111b
Wu, Xiaochang <xiaochang.wu@intel.com>
2021-03-31 15:59:53 +0800

    add setup-python3 and setup-cluster, fix paths

--- dev/test-cluster/test-cluster.sh
+++ dev/test-cluster/test-cluster.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-cd $GITHUB_WORKSPACE
+# Setup Python3 and Spark cluster
+cd $GITHUB_WORKSPACE/dev/test-cluster
+./setup-python3-env.sh
+./setup-cluster.sh
 
-source ./env.sh
+# Build and run all examples
+source $GITHUB_WORKSPACE/dev/test-cluster/env.sh
 
-cd examples
+cd $GITHUB_WORKSPACE/examples
 
 hadoop fs -copyFromLocal data
 hadoop fs -ls data
 
-./build.sh
+./build-all.sh
 ./run-all-scala.sh
 ./run-all-pyspark.sh
