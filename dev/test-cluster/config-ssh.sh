bd7eab25cc12aa0d06fb06bd1a254d8a2cfdfe56
Wu, Xiaochang <xiaochang.wu@intel.com>
2021-04-01 10:10:08 +0800

    set strict modes no

--- dev/test-cluster/config-ssh.sh
+++ dev/test-cluster/config-ssh.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 
-set -x
-
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-
 ssh-keygen -q -N "" -t rsa -f ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
+# Disable strict host key checking
 echo "    StrictHostKeyChecking no                     " | sudo tee -a /etc/ssh/ssh_config
+# Disable strict modes for less strict permission checking
+echo "StrictModes no" | sudo tee -a /etc/ssh/sshd_config
 
 ls -ld ~/.ssh
 ls -l ~/.ssh
 
-sudo systemctl restart sshd
+sudo systemctl restart ssh
 
-# ssh -vvv localhost
-ssh -vvv `hostname`
+ssh localhost
