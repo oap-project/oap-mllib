59cddfc7d3ca6a1b05f420983a04aea583be4e2e
Wu, Xiaochang <xiaochang.wu@intel.com>
2021-04-12 16:04:30 +0800

    nit

--- dev/install-build-deps-centos.sh
+++ dev/install-build-deps-centos.sh
@@ -13,7 +13,7 @@ repo_gpgcheck=1
 gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 EOF
   sudo mv /tmp/oneAPI.repo /etc/yum.repos.d
-  sudo yum install -y intel-basekit-2021.2.0
+  sudo yum install -y intel-oneapi-dpcpp-cpp-2021.2.0 intel-oneapi-dal-devel-2021.2.0 intel-oneapi-tbb-devel-2021.2.0 intel-oneapi-ccl-devel-2021.2.0
 else
   echo "oneAPI components already installed!"
 fi  
--- dev/install-build-deps-ubuntu.sh
+++ dev/install-build-deps-ubuntu.sh
@@ -8,7 +8,7 @@ if [ ! -f /opt/intel/oneapi ]; then
   rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
   echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
   sudo apt-get update
-  sudo apt-get install -y intel-basekit-2021.2.0
+  sudo apt-get install -y intel-oneapi-dpcpp-cpp-2021.2.0 intel-oneapi-dal-devel-2021.2.0 intel-oneapi-tbb-devel-2021.2.0 intel-oneapi-ccl-devel-2021.2.0
 else
   echo "oneAPI components already installed!"
 fi  
