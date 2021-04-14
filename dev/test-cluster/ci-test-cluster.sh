30ab18bb47b09cb1a9a571c9df3fc016b34f253c
Xiaochang Wu <xiaochang.wu@intel.com>
2021-04-14 18:34:29 +0800

    update

--- dev/test-cluster/ci-test-cluster.sh
+++ dev/test-cluster/ci-test-cluster.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
-
-# Setup Password-less & Python3
-cd $GITHUB_WORKSPACE/dev/test-cluster
-./config-ssh.sh
-./setup-python3.sh
-
-# Setup Hadoop cluster and envs
-# source ./setup-cluster.sh
-
 # Build and run all examples
 cp $GITHUB_WORKSPACE/dev/test-cluster/env.sh $GITHUB_WORKSPACE/conf
 
