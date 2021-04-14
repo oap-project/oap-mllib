4e1b138e5397b36e4f75dabf861f5c954956ffda
Xiaochang Wu <xiaochang.wu@intel.com>
2021-04-14 21:30:26 +0800

    update

--- dev/setup-all.sh
+++ dev/setup-all.sh
@@ -3,13 +3,12 @@
 # Setup hosts
 echo "$(hostname -i) $(hostname)" | sudo tee -a /etc/hosts
 
-# Install all building dependencies
-${{github.workspace}}/dev/install-build-deps-ubuntu.sh
+# Install dependencies for building
+$GITHUB_WORKSPACE/dev/install-build-deps-ubuntu.sh
 
-# Setup password-less & Python3
-cd $GITHUB_WORKSPACE/dev/test-cluster
-./config-ssh.sh
-./setup-python3.sh
+# Setup password-less & python3
+$GITHUB_WORKSPACE/dev/test-cluster/config-ssh.sh
+$GITHUB_WORKSPACE/dev/test-cluster/setup-python3.sh
 
-# Setup Hadoop cluster and envs
-source ./setup-cluster.sh
+# Setup cluster and envs
+source $GITHUB_WORKSPACE/dev/test-cluster/setup-cluster.sh
