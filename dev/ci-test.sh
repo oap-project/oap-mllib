d5ff2e11e08d3f54940fd90a52c888ef823ed442
Jiang, Bo <bo.b.jiang@intel.com>
2021-04-26 13:17:16 +0800

    update ci-test to support profile

--- dev/ci-test.sh
+++ dev/ci-test.sh
@@ -30,6 +30,12 @@ if [[ -z $CCL_ROOT ]]; then
  exit 1
 fi
 
+if [[ -z $SPARK_VER ]]; then
+ echo SPARK_VER not defined, using default.
+else
+ SPARK_VER=$SPARK_VER
+fi
+
 echo === Testing Environments ===
 echo JAVA_HOME=$JAVA_HOME
 echo DAALROOT=$DAALROOT
@@ -37,6 +43,8 @@ echo TBBROOT=$TBBROOT
 echo CCL_ROOT=$CCL_ROOT
 echo Maven Version: $(mvn -v | head -n 1 | cut -f3 -d" ")
 echo Clang Version: $(clang -dumpversion)
+echo SPARK_VER=$SPARK_VER
+echo GCC Version: $(gcc -dumpversion)
 echo =============================
 
 cd $GITHUB_WORKSPACE/mllib-dal
@@ -53,9 +61,15 @@ $GITHUB_WORKSPACE/dev/ci-build.sh
 # mvn -Dtest=none -Dmaven.test.skip=false test
 
 # Individual test
-mvn --no-transfer-progress -Dtest=none -DwildcardSuites=org.apache.spark.ml.clustering.IntelKMeansSuite test
-mvn --no-transfer-progress -Dtest=none -DwildcardSuites=org.apache.spark.ml.feature.IntelPCASuite test
+if [[ -z $SPARK_VER ]]; then
+ mvn --no-transfer-progress -Dtest=none -DwildcardSuites=org.apache.spark.ml.clustering.IntelKMeansSuite test
+ mvn --no-transfer-progress -Dtest=none -DwildcardSuites=org.apache.spark.ml.feature.IntelPCASuite test
 # mvn -Dtest=none -DwildcardSuites=org.apache.spark.ml.recommendation.IntelALSSuite test
+else
+ mvn --no-transfer-progress -Dtest=none -DwildcardSuites=org.apache.spark.ml.clustering.IntelKMeansSuite test -P$SPARK_VER
+ mvn --no-transfer-progress -Dtest=none -DwildcardSuites=org.apache.spark.ml.feature.IntelPCASuite test -P$SPARK_VER
+# mvn -Dtest=none -DwildcardSuites=org.apache.spark.ml.recommendation.IntelALSSuite test -P$SPARK_VER
+fi
 
 # Yarn cluster test
 $GITHUB_WORKSPACE/dev/test-cluster/ci-test-cluster.sh
