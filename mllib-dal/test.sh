cda02510c6ae1566f871662e8609269c796b963c
Wu, Xiaochang <xiaochang.wu@intel.com>
2021-05-10 20:23:38 +0800

    Update tests

--- mllib-dal/test.sh
+++ mllib-dal/test.sh
@@ -42,13 +42,16 @@ echo Clang Version: $(clang -dumpversion)
 echo SPARK_VER=$SPARK_VER
 echo =============================
 
+# Clean
+mvn clean
+
 # Individual test
 if [[ -z $SPARK_VER ]]; then
- mvn -Dtest=none -DwildcardSuites=org.apache.spark.ml.clustering.IntelKMeansSuite clean test
- mvn -Dtest=none -DwildcardSuites=org.apache.spark.ml.feature.IntelPCASuite clean test
-# mvn -Dtest=none -DwildcardSuites=org.apache.spark.ml.recommendation.IntelALSSuite clean test
+ mvn -Dtest=none -DwildcardSuites=org.apache.spark.ml.clustering.IntelKMeansSuite test
+ mvn -Dtest=none -DwildcardSuites=org.apache.spark.ml.feature.IntelPCASuite test
+# mvn -Dtest=none -DwildcardSuites=org.apache.spark.ml.recommendation.IntelALSSuite test
 else
- mvn -P$SPARK_VER -Dtest=none -DwildcardSuites=org.apache.spark.ml.clustering.IntelKMeansSuite clean test
- mvn -P$SPARK_VER -Dtest=none -DwildcardSuites=org.apache.spark.ml.feature.IntelPCASuite clean test
-# mvn -P$SPARK_VER -Dtest=none -DwildcardSuites=org.apache.spark.ml.recommendation.IntelALSSuite clean test
+ mvn -P$SPARK_VER -Dtest=none -DwildcardSuites=org.apache.spark.ml.clustering.IntelKMeansSuite test
+ mvn -P$SPARK_VER -Dtest=none -DwildcardSuites=org.apache.spark.ml.feature.IntelPCASuite test
+# mvn -P$SPARK_VER -Dtest=none -DwildcardSuites=org.apache.spark.ml.recommendation.IntelALSSuite test
 fi
