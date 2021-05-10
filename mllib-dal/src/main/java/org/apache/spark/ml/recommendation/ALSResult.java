2e6327eb8c6e0938ae4e23ee16f3c716ef4601ec
Wu, Xiaochang <xiaochang.wu@intel.com>
2021-05-10 16:36:24 +0800

    ALS: Fix rankId missing

--- mllib-dal/src/main/java/org/apache/spark/ml/recommendation/ALSResult.java
+++ mllib-dal/src/main/java/org/apache/spark/ml/recommendation/ALSResult.java
@@ -17,6 +17,7 @@
 package org.apache.spark.ml.recommendation;
 
 public class ALSResult {
+  public long rankId = -1;
   public long cUsersFactorsNumTab;
   public long cItemsFactorsNumTab;
   public long cUserOffset;
