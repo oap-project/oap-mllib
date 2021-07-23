bacdb85e0144f51bd3e6f1bd58d4bd602431d67a
Wu, Xiaochang <xiaochang.wu@intel.com>
2021-07-23 22:15:00 +0800

    update to oneapi 2021.3

--- dev/install-build-deps-ubuntu.sh
+++ dev/install-build-deps-ubuntu.sh
@@ -9,7 +9,7 @@ if [ ! -d /opt/intel/oneapi ]; then
   echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
   sudo apt-get update
   # sudo apt-get install -y build-essential cmake
-  sudo apt-get install -y intel-oneapi-dpcpp-cpp-2021.2.0 intel-oneapi-dal-devel-2021.2.0 intel-oneapi-tbb-devel-2021.2.0
+  sudo apt-get install -y intel-oneapi-dpcpp-cpp-2021.3.0 intel-oneapi-dal-devel-2021.3.0 intel-oneapi-tbb-devel-2021.3.0
 else
   echo "oneAPI components already installed!"
 fi
@@ -19,7 +19,7 @@ cd /tmp
 rm -rf oneCCL
 git clone https://github.com/oneapi-src/oneCCL
 cd oneCCL
-git checkout 2021.2
+git checkout 2021.3
 mkdir build && cd build
 cmake ..
 make -j 2 install
