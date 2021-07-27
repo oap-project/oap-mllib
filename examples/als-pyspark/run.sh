bdcbebb3c7314b094f8038ee9fe442475a8cd921
Xiaochang Wu <xiaochang.wu@intel.com>
2021-07-27 11:55:12 +0800

    [ML-101] [Release] Add version update scripts and improve scripts for examples (#102)
    * add set version script and update example versions
    
    * improve example test scripts
    
    * update

--- examples/als-pyspark/run.sh
+++ examples/als-pyspark/run.sh
@@ -4,14 +4,15 @@ source ../../conf/env.sh
 
 # Data file is converted from oneDAL examples ($DAALROOT/examples/daal/data/batch/implicit_als_csr.csv)
 # The data file should be copied to $HDFS_ROOT before running examples
-DATA_FILE=data/onedal_als_csr_ratings.txt
+DATA_FILE=$HDFS_ROOT/data/onedal_als_csr_ratings.txt
 
 APP_PY=als-pyspark.py
 
 time $SPARK_HOME/bin/spark-submit --master $SPARK_MASTER -v \
     --num-executors $SPARK_NUM_EXECUTORS \
-    --driver-memory $SPARK_DRIVER_MEMORY \
     --executor-cores $SPARK_EXECUTOR_CORES \
+    --total-executor-cores $SPARK_TOTAL_CORES \
+    --driver-memory $SPARK_DRIVER_MEMORY \
     --executor-memory $SPARK_EXECUTOR_MEMORY \
     --conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" \
     --conf "spark.default.parallelism=$SPARK_DEFAULT_PARALLELISM" \
