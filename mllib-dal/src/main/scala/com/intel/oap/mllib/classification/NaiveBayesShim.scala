a5f67e34df9a59b0d6db601e46e3a5d81216a9aa
minmingzhu <minming.zhu@intel.com>
2022-03-23 17:26:26 +0800

    support spark 3.1.3
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/scala/com/intel/oap/mllib/classification/NaiveBayesShim.scala
+++ mllib-dal/src/main/scala/com/intel/oap/mllib/classification/NaiveBayesShim.scala
@@ -32,7 +32,7 @@ object NaiveBayesShim extends Logging {
   def create(uid: String): NaiveBayesShim = {
     logInfo(s"Loading NaiveBayes for Spark $SPARK_VERSION")
     val shim = SPARK_VERSION match {
-      case "3.1.1" | "3.1.2" | "3.2.0" => new NaiveBayesSpark320(uid)
+      case "3.1.1" | "3.1.2" | "3.1.3" | "3.2.0" => new NaiveBayesSpark320(uid)
       case _ => throw new SparkException(s"Unsupported Spark version $SPARK_VERSION")
     }
     shim
--- mllib-dal/src/main/scala/com/intel/oap/mllib/clustering/KMeansShim.scala
+++ mllib-dal/src/main/scala/com/intel/oap/mllib/clustering/KMeansShim.scala
@@ -32,7 +32,7 @@ object KMeansShim extends Logging {
   def create(uid: String): KMeansShim = {
     logInfo(s"Loading KMeans for Spark $SPARK_VERSION")
     val kmeans = SPARK_VERSION match {
-      case "3.1.1" | "3.1.2" | "3.2.0" => new KMeansSpark320(uid)
+      case "3.1.1" | "3.1.2" | "3.1.3" | "3.2.0" => new KMeansSpark320(uid)
       case _ => throw new SparkException(s"Unsupported Spark version $SPARK_VERSION")
     }
     kmeans
