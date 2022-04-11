ca7fe3a8dd896bb877ce55e758bd1a65f560734a
minmingzhu <45281494+minmingzhu@users.noreply.github.com>
2022-04-11 09:49:31 +0800

    Update install-build-deps-ubuntu.sh

--- dev/install-build-deps-ubuntu.sh
+++ dev/install-build-deps-ubuntu.sh
@@ -7,10 +7,10 @@ if [ ! -d /opt/intel/oneapi ]; then
   sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
   rm GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
   echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+  cat /etc/apt/sources.list.d/oneAPI.list
   sudo apt-get update
-  ls /var/lib/apt/lists/intel-oneapi* | sed -E 's/.*\/([^\/_]*).*/\1/' | sort -u
   # sudo apt-get install -y build-essential cmake
-  sudo apt-get install -y intel-oneapi-python.x86_64 intel-oneapi-dpcpp-cpp-2021.4.0 intel-oneapi-dal-devel-2021.4.0 intel-oneapi-tbb-devel-2021.4.0 intel-oneapi-ccl-devel-2021.4.0 intel-oneapi-mpi-devel-2021.4.0
+  sudo apt-get install -y intel-oneapi-python* intel-oneapi-dpcpp-cpp-2021.4.0 intel-oneapi-dal-devel-2021.4.0 intel-oneapi-tbb-devel-2021.4.0 intel-oneapi-ccl-devel-2021.4.0 intel-oneapi-mpi-devel-2021.4.0
 else
   echo "oneAPI components already installed!"
 fi
