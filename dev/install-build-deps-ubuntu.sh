439ed5ac5179f8a2113e3e80aa17eaf58d857464
minmingz <minming.zhu@inte.com>
2022-04-13 15:05:05 +0800

    update ci
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/install-build-deps-ubuntu.sh
+++ dev/install-build-deps-ubuntu.sh
@@ -3,9 +3,9 @@
 if [ ! -d /opt/intel/oneapi ]; then
   echo "Installing oneAPI components ..."
   cd /tmp
-  wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-  sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-  rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+  wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+  sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+  rm GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
   echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
   sudo apt-get update
   # sudo apt-get install -y build-essential cmake
