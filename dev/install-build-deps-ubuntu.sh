59408bb40c94d69f3baa6dd66e2f30679d968522
minmingz <minming.zhu@inte.com>
2022-04-13 22:56:26 +0800

    update ci conf
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/install-build-deps-ubuntu.sh
+++ dev/install-build-deps-ubuntu.sh
@@ -3,13 +3,13 @@
 if [ ! -d /opt/intel/oneapi ]; then
   echo "Installing oneAPI components ..."
   cd /tmp
-  wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-  sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-  rm GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+  wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+  sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+  rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
   echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
   sudo apt-get update
   # sudo apt-get install -y build-essential cmake
-  sudo apt-get install -y intel-basekit
+  sudo apt-get install -y intel-oneapi-dpcpp-cpp-2021.4.0 intel-oneapi-dal-devel-2021.4.0 intel-oneapi-tbb-devel-2021.4.0 intel-oneapi-ccl-devel-2021.4.0 intel-oneapi-mpi-devel-2021.4.0
 else
   echo "oneAPI components already installed!"
 fi
