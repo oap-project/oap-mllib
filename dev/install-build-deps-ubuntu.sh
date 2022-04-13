1046f4af6930550f159034e18065a2637a3435db
minmingz <minming.zhu@inte.com>
2022-04-13 15:12:25 +0800

    update ci
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/install-build-deps-ubuntu.sh
+++ dev/install-build-deps-ubuntu.sh
@@ -9,7 +9,7 @@ if [ ! -d /opt/intel/oneapi ]; then
   echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
   sudo apt-get update
   # sudo apt-get install -y build-essential cmake
-  sudo apt-get install -y intel-oneapi-dpcpp-cpp-2022.1.2 intel-oneapi-dal-devel-2022.1.2 intel-oneapi-tbb-devel-2022.1.2 intel-oneapi-ccl-devel-2022.1.2 intel-oneapi-mpi-devel-2022.1.2
+  sudo apt-get install -y intel-basekit
 else
   echo "oneAPI components already installed!"
 fi
