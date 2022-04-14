774be67b182efea78798058d972fca3cb10f16c2
minmingzhu <minming.zhu@intel.com>
2022-04-14 15:18:14 +0800

    update ci
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/install-build-deps-ubuntu.sh
+++ dev/install-build-deps-ubuntu.sh
@@ -9,7 +9,6 @@ if [ ! -d /opt/intel/oneapi ]; then
 
   # add signed entry to apt sources and configure the APT client to use Intel repository:
   echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
-
   sudo apt-get update
   # sudo apt-get install -y build-essential cmake
   sudo apt-get install -y intel-oneapi-dpcpp-cpp-2022.0.2 intel-oneapi-dal-devel-2021.5.3  intel-oneapi-tbb-devel-2021.5.1 intel-oneapi-ccl-devel-2021.5.1 intel-oneapi-mpi-devel-2021.5.1
