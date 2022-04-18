6392925d5ca455956e5ab97ffa9feaf51d1d8ddb
minmingzhu <minming.zhu@intel.com>
2022-04-18 19:44:31 +0800

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/install-build-deps-ubuntu.sh
+++ dev/install-build-deps-ubuntu.sh
@@ -3,9 +3,9 @@
 if [ ! -d /opt/intel/oneapi ]; then
   echo "Installing oneAPI components ..."
   cd /tmp
-  wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-  sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-  rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+  wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+  sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+  rm GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
   sudo apt-get update
   # sudo apt-get install -y build-essential cmake
   sudo apt-get install -y intel-oneapi-dpcpp-cpp-2022.1.2 intel-oneapi-dal-devel-2022.1.2 intel-oneapi-tbb-devel-2022.1.2 intel-oneapi-ccl-devel-2022.1.2 intel-oneapi-mpi-devel-2022.1.2
