0923b4a6e7eea234b7382b0b95dc44b5010ab3cf
minmingzhu <minming.zhu@intel.com>
2022-04-19 14:49:58 +0800

    update ci
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/ci/ci-local-dpc-test.sh
+++ dev/ci/ci-local-dpc-test.sh
@@ -8,6 +8,9 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
+# install GPU Device
+apt-get install intel-opencl-icd
+
 # Install dependencies for building
 $GITHUB_WORKSPACE/dev/install-build-deps-ubuntu.sh
 
@@ -16,7 +19,8 @@ source /opt/intel/oneapi/setvars.sh
 
 # Prepare lib resources
 cd $GITHUB_WORKSPACE/mllib-dal
-../dev/prepare-build-deps.sh
-./build.sh -p CPU_ONLY_PROFILE -q
+../dev/prepare-build-deps-gpu.sh
+./build.sh -p CPU_GPU_PROFILE -q
 
-./test.sh -p CPU_ONLY_PROFILE -q
+unset LD_LIBRARY_PATH
+./test.sh -p CPU_GPU_PROFILE -q
