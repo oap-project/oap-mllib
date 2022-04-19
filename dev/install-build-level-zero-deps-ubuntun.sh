e80a9edc9fd6d1d790e0785914edb56672334bed
minmingzhu <minming.zhu@intel.com>
2022-04-19 22:41:58 +0800

    update ci
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/install-build-level-zero-deps-ubuntun.sh
+++ dev/install-build-level-zero-deps-ubuntun.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 echo "Installing oneAPI components ..."
-wget https://github.com/oneapi-src/level-zero.git
+git clone https://github.com/oneapi-src/level-zero.git
 cd level-zero
 git checkout v1.7.15
 mkdir build
 cd build
 cmake ..
-cmake --build . --config Release
-cmake --build . --config Release --target package
-cmake --build . --config Release --target install
+sudo cmake --build . --config Release
+sudo cmake --build . --config Release --target package
+sudo cmake --build . --config Release --target install
 cd ../
-echo "#!/bin/bash \n export L0T_ROOT=$(dirname $(realpath ${BASH_SOURCE})) \n
-      export LD_LIBRARY_PATH=${L0T_ROOT}/build/lib:${LD_LIBRARY_PATH:-} \n" > setvars.sh
+echo -e "#!/bin/bash \nexport L0T_ROOT=$(dirname $(realpath ${BASH_SOURCE})) \nexport LD_LIBRARY_PATH=${L0T_ROOT}/build/lib:${LD_LIBRARY_PATH:-} \n" > setvars.sh
