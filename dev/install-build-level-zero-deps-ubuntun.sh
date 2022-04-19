d7a334a1a310a6b9528f0139916a5a7a634d7dce
minmingzhu <45281494+minmingzhu@users.noreply.github.com>
2022-04-19 22:49:46 +0800

    Update install-build-level-zero-deps-ubuntun.sh

--- dev/install-build-level-zero-deps-ubuntun.sh
+++ dev/install-build-level-zero-deps-ubuntun.sh
@@ -10,4 +10,5 @@ sudo cmake --build . --config Release
 sudo cmake --build . --config Release --target package
 sudo cmake --build . --config Release --target install
 cd ../
-echo -e "#!/bin/bash \nexport L0T_ROOT=$(dirname $(realpath ${BASH_SOURCE})) \nexport LD_LIBRARY_PATH=${L0T_ROOT}/build/lib:${LD_LIBRARY_PATH:-} \n" > setvars.sh
+echo -e "#!/bin/bash \n export L0T_ROOT=$(dirname $(realpath ${BASH_SOURCE})) \n export LD_LIBRARY_PATH=${L0T_ROOT}/build/lib:${LD_LIBRARY_PATH:-} " > setvars.sh
+cat setvars.sh
