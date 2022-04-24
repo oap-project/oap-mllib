99a958f0fde53418c833714e8ac05b6ebe11fa51
minmingzhu <45281494+minmingzhu@users.noreply.github.com>
2022-04-24 18:17:25 +0800

    Update ci-local-dpc-test.sh

--- dev/ci/ci-local-dpc-test.sh
+++ dev/ci/ci-local-dpc-test.sh
@@ -28,7 +28,7 @@ cd  $GITHUB_WORKSPACE/dev/tools/list-compute-devices/
 ./run.sh
 
 # Prepare lib resources
-cd $GITHUB_WORKSPACE/mllib-da
+cd $GITHUB_WORKSPACE/mllib-dal
 ../dev/prepare-build-deps-gpu.sh
 ./build.sh -p CPU_ONLY_PROFILE
 
