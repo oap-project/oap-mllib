caa4d7157d1a2a7ae5b9005f71b8d06eba40b1d7
minmingzhu <45281494+minmingzhu@users.noreply.github.com>
2022-04-24 18:17:25 +0800

    Update ci-local-dpc-test.sh

--- dev/ci/ci-local-dpc-test.sh
+++ dev/ci/ci-local-dpc-test.sh
@@ -25,7 +25,7 @@ cd  $GITHUB_WORKSPACE/dev/tools/check-gpu-cpu/
 ./run.sh
 
 # Prepare lib resources
-cd $GITHUB_WORKSPACE/mllib-da
+cd $GITHUB_WORKSPACE/mllib-dal
 ../dev/prepare-build-deps-gpu.sh
 ./build.sh -p CPU_GPU_PROFILE
 
