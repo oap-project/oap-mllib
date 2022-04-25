3a439e61e75f78a3eeeb7dc27e335d1fbe28ce02
minmingzhu <minming.zhu@intel.com>
2022-04-25 09:46:09 +0800

    update ci
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/ci/ci-local-dpc-test.sh
+++ dev/ci/ci-local-dpc-test.sh
@@ -30,7 +30,7 @@ cd  $GITHUB_WORKSPACE/dev/tools/list-compute-devices/
 # Prepare lib resources
 cd $GITHUB_WORKSPACE/mllib-da
 ../dev/prepare-build-deps-gpu.sh
-./build.sh -p CPU_GPU_PROFILE
+./build.sh -p CPU_ONLY_PROFILE
 
 unset LD_LIBRARY_PATH
 ./test.sh -p CPU_GPU_PROFILE -q -d host
