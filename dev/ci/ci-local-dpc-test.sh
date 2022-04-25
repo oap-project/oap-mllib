a075d1152a59aad17867fed10afd859512087ffc
minmingzhu <minming.zhu@intel.com>
2022-04-25 11:59:29 +0800

    update ci
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/ci/ci-local-dpc-test.sh
+++ dev/ci/ci-local-dpc-test.sh
@@ -8,9 +8,6 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-lscpu
-
-#$GITHUB_WORKSPACE/dev/install-build-level-zero-deps-ubuntun.sh
 $GITHUB_WORKSPACE/dev/install-level-zero-ubuntu.sh
 
 # Install dependencies for building
@@ -18,7 +15,6 @@ $GITHUB_WORKSPACE/dev/install-build-deps-ubuntu.sh
 
 # Setup building envs
 source /opt/intel/oneapi/setvars.sh
-#source /home/runner/work/level-zero/setvars.sh
 
 cd  $GITHUB_WORKSPACE/dev/tools/check-gpu-cpu/
 ./build.sh
