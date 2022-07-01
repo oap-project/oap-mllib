614ca77f429a89ab669616588705a6a16fa8739f
minmingzhu <45281494+minmingzhu@users.noreply.github.com>
2022-07-01 14:19:16 +0800

    Update ci-cluster-test.sh

--- dev/ci/ci-cluster-test.sh
+++ dev/ci/ci-cluster-test.sh
@@ -8,7 +8,6 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-
 # Install dependencies for building
 $GITHUB_WORKSPACE/dev/install-build-deps-ubuntu.sh
 
