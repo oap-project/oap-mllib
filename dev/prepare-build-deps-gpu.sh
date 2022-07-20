3a165b31b4e1ef249c5a5944cfa1c5c41e8eaed5
minmingzhu <minming.zhu@intel.com>
2022-07-20 08:40:05 +0000

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/prepare-build-deps-gpu.sh
+++ dev/prepare-build-deps-gpu.sh
@@ -62,8 +62,8 @@ cp $I_MPI_ROOT/lib/release_mt/libmpi.so.12.0.0 $TARGET_DIR/libmpi.so.12
 
 cp $DAALROOT/lib/intel64/libJavaAPI.so.1.1 $TARGET_DIR/libJavaAPI.so
 
-cp $TBBROOT/lib/intel64/gcc4.8/libtbb.so.12.6 $TARGET_DIR/libtbb.so.12
-cp $TBBROOT/lib/intel64/gcc4.8/libtbbmalloc.so.2.6 $TARGET_DIR/libtbbmalloc.so.2
+cp $TBBROOT/lib/intel64/gcc4.8/libtbb.so.12.5 $TARGET_DIR/libtbb.so.12
+cp $TBBROOT/lib/intel64/gcc4.8/libtbbmalloc.so.2.5 $TARGET_DIR/libtbbmalloc.so.2
 
 # SYCL libs
 cp $CMPLR_ROOT/linux/compiler/lib/intel64_lin/libintlc.so.5 $TARGET_DIR
