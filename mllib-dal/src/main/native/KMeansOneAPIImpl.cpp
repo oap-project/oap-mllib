86eeae4efc40999883b254158d7b35373b6307a6
minmingz <minming.zhu@inte.com>
2023-03-13 07:49:39 +0000

    enable kmeans GPU and CPU
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/native/KMeansOneAPIImpl.cpp
+++ mllib-dal/src/main/native/KMeansOneAPIImpl.cpp
@@ -23,28 +23,237 @@
 #ifndef ONEDAL_DATA_PARALLEL
 #define ONEDAL_DATA_PARALLEL
 #endif
-
 #include "Communicator.hpp"
-#include "com_intel_oap_mllib_clustering_KMeansDALImpl.h"
 #include "oneapi/dal/algo/kmeans.hpp"
 #include "oneapi/dal/table/homogen.hpp"
+#endif
+
+#include "com_intel_oap_mllib_clustering_KMeansDALImpl.h"
 #include "service.h"
+#include "OneCCL.h"
 
 using namespace std;
+#ifdef CPU_GPU_PROFILE
 using namespace oneapi::dal;
-const int ccl_root = 0;
+#else
+using namespace daal;
+using namespace daal::algorithms;
+using namespace daal::services;
+#endif
 
+#ifdef CPU_ONLY_PROFILE
+typedef double algorithmFPType; /* Algorithm floating-point type */
+
+static NumericTablePtr kmeans_compute(int rankId, ccl::communicator &comm,
+                                      const NumericTablePtr &pData,
+                                      const NumericTablePtr &initialCentroids,
+                                      size_t nClusters, size_t nBlocks,
+                                      algorithmFPType &ret_cost) {
+    const bool isRoot = (rankId == ccl_root);
+    size_t CentroidsArchLength = 0;
+    InputDataArchive inputArch;
+    if (isRoot) {
+        /* Retrieve the algorithm results and serialize them */
+        initialCentroids->serialize(inputArch);
+        CentroidsArchLength = inputArch.getSizeOfArchive();
+    }
+
+    /* Get partial results from the root node */
+    ccl::broadcast(&CentroidsArchLength, sizeof(size_t), ccl::datatype::uint8,
+                   ccl_root, comm)
+        .wait();
+
+    ByteBuffer nodeCentroids(CentroidsArchLength);
+    if (isRoot)
+        inputArch.copyArchiveToArray(&nodeCentroids[0], CentroidsArchLength);
+
+    ccl::broadcast(&nodeCentroids[0], CentroidsArchLength, ccl::datatype::uint8,
+                   ccl_root, comm)
+        .wait();
+
+    /* Deserialize centroids data */
+    OutputDataArchive outArch(nodeCentroids.size() ? &nodeCentroids[0] : NULL,
+                              CentroidsArchLength);
+
+    NumericTablePtr centroids(new HomogenNumericTable<algorithmFPType>());
+
+    centroids->deserialize(outArch);
+
+    /* Create an algorithm to compute k-means on local nodes */
+    kmeans::Distributed<step1Local, algorithmFPType> localAlgorithm(nClusters);
+
+    /* Set the input data set to the algorithm */
+    localAlgorithm.input.set(kmeans::data, pData);
+    localAlgorithm.input.set(kmeans::inputCentroids, centroids);
+
+    /* Compute k-means */
+    localAlgorithm.compute();
+
+    /* Serialize partial results required by step 2 */
+    InputDataArchive dataArch;
+    localAlgorithm.getPartialResult()->serialize(dataArch);
+    size_t perNodeArchLength = dataArch.getSizeOfArchive();
+    ByteBuffer serializedData;
+
+    /* Serialized data is of equal size on each node if each node called
+     * compute() equal number of times */
+    vector<size_t> recvCounts(nBlocks);
+    for (size_t i = 0; i < nBlocks; i++) {
+        recvCounts[i] = perNodeArchLength;
+    }
+    serializedData.resize(perNodeArchLength * nBlocks);
+
+    ByteBuffer nodeResults(perNodeArchLength);
+    dataArch.copyArchiveToArray(&nodeResults[0], perNodeArchLength);
+
+    /* Transfer partial results to step 2 on the root node */
+    ccl::allgatherv(&nodeResults[0], perNodeArchLength, &serializedData[0],
+                    recvCounts, ccl::datatype::uint8, comm)
+        .wait();
+
+    if (isRoot) {
+        /* Create an algorithm to compute k-means on the master node */
+        kmeans::Distributed<step2Master, algorithmFPType> masterAlgorithm(
+            nClusters);
+
+        for (size_t i = 0; i < nBlocks; i++) {
+            /* Deserialize partial results from step 1 */
+            OutputDataArchive dataArch(&serializedData[perNodeArchLength * i],
+                                       perNodeArchLength);
+
+            kmeans::PartialResultPtr dataForStep2FromStep1(
+                new kmeans::PartialResult());
+            dataForStep2FromStep1->deserialize(dataArch);
+
+            /* Set local partial results as input for the master-node algorithm
+             */
+            masterAlgorithm.input.add(kmeans::partialResults,
+                                      dataForStep2FromStep1);
+        }
+
+        /* Merge and finalizeCompute k-means on the master node */
+        masterAlgorithm.compute();
+        masterAlgorithm.finalizeCompute();
+
+        ret_cost = masterAlgorithm.getResult()
+                       ->get(kmeans::objectiveFunction)
+                       ->getValue<algorithmFPType>(0, 0);
+
+        /* Retrieve the algorithm results */
+        return masterAlgorithm.getResult()->get(kmeans::centroids);
+    }
+    return NumericTablePtr();
+}
+
+static bool isCenterConverged(const algorithmFPType *oldCenter,
+                              const algorithmFPType *newCenter, size_t dim,
+                              double tolerance) {
+
+    algorithmFPType sums = 0.0;
+
+    for (size_t i = 0; i < dim; i++)
+        sums += (newCenter[i] - oldCenter[i]) * (newCenter[i] - oldCenter[i]);
+
+    return sums <= tolerance * tolerance;
+}
+
+static bool areAllCentersConverged(const NumericTablePtr &oldCenters,
+                                   const NumericTablePtr &newCenters,
+                                   double tolerance) {
+    size_t rows = oldCenters->getNumberOfRows();
+    size_t cols = oldCenters->getNumberOfColumns();
+
+    BlockDescriptor<algorithmFPType> blockOldCenters;
+    oldCenters->getBlockOfRows(0, rows, readOnly, blockOldCenters);
+    algorithmFPType *arrayOldCenters = blockOldCenters.getBlockPtr();
+
+    BlockDescriptor<algorithmFPType> blockNewCenters;
+    newCenters->getBlockOfRows(0, rows, readOnly, blockNewCenters);
+    algorithmFPType *arrayNewCenters = blockNewCenters.getBlockPtr();
+
+    for (size_t i = 0; i < rows; i++) {
+        if (!isCenterConverged(&arrayOldCenters[i * cols],
+                               &arrayNewCenters[i * cols], cols, tolerance))
+            return false;
+    }
+
+    return true;
+}
+
+static jlong doKMeansDaalCompute(
+    JNIEnv *env, jobject obj, int rankId, ccl::communicator &comm,
+    NumericTablePtr &pData, NumericTablePtr &centroids, jint cluster_num,
+    jdouble tolerance, jint iteration_num, jint executor_num,
+    jobject resultObj) {
+
+    algorithmFPType totalCost;
+
+    NumericTablePtr newCentroids;
+    bool converged = false;
+
+    int it = 0;
+    for (it = 0; it < iteration_num && !converged; it++) {
+        auto t1 = std::chrono::high_resolution_clock::now();
+
+        newCentroids = kmeans_compute(rankId, comm, pData, centroids,
+                                      cluster_num, executor_num, totalCost);
+
+        if (rankId == ccl_root) {
+            converged =
+                areAllCentersConverged(centroids, newCentroids, tolerance);
+        }
+
+        // Sync converged status
+        ccl::broadcast(&converged, 1, ccl::datatype::uint8, ccl_root, comm)
+            .wait();
+
+        centroids = newCentroids;
+
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration =
+            std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count();
+        std::cout << "KMeans (native): iteration " << it << " took " << duration
+                  << " secs" << std::endl;
+    }
+
+    if (rankId == ccl_root) {
+        if (it == iteration_num)
+            std::cout << "KMeans (native): reached " << iteration_num
+                      << " max iterations." << std::endl;
+        else
+            std::cout << "KMeans (native): converged in " << it
+                      << " iterations." << std::endl;
+
+        // Get the class of the input object
+        jclass clazz = env->GetObjectClass(resultObj);
+        // Get Field references
+        jfieldID totalCostField = env->GetFieldID(clazz, "totalCost", "D");
+        jfieldID iterationNumField =
+            env->GetFieldID(clazz, "iterationNum", "I");
+
+        // Set iteration num for result
+        env->SetIntField(resultObj, iterationNumField, it);
+        // Set cost for result
+        env->SetDoubleField(resultObj, totalCostField, totalCost);
+
+        NumericTablePtr *ret = new NumericTablePtr(centroids);
+        return (jlong)ret;
+    } else {
+        return (jlong)0;
+    }
+}
+#endif
+
+#ifdef CPU_GPU_PROFILE
 static jlong doKMeansOneAPICompute(JNIEnv *env, jint rankId, jlong pNumTabData,
                                    jlong pNumTabCenters, jint clusterNum,
                                    jdouble tolerance, jint iterationNum,
                                    jint executorNum, const ccl::string &ipPort,
-                                   jint computeDeviceOrdinal,
+                                   ComputeDevice &device,
                                    jobject resultObj) {
     std::cout << "oneDAL (native): compute start , rankid = " << rankId
-              << "; device = " << ComputeDeviceString[computeDeviceOrdinal]
               << std::endl;
     const bool isRoot = (rankId == ccl_root);
-    ComputeDevice device = getComputeDeviceByOrdinal(computeDeviceOrdinal);
     homogen_table htable =
         *reinterpret_cast<const homogen_table *>(pNumTabData);
     homogen_table centroids =
@@ -55,10 +264,16 @@ static jlong doKMeansOneAPICompute(JNIEnv *env, jint rankId, jlong pNumTabData,
                                  .set_accuracy_threshold(tolerance);
     kmeans::train_input local_input{htable, centroids};
     auto queue = getQueue(device);
+    std::cout << "kmeans oneapi training 1"
+                  << std::endl;
     auto comm = preview::spmd::make_communicator<preview::spmd::backend::ccl>(
         queue, executorNum, rankId, ipPort);
+    std::cout << "kmeans oneapi training 2"
+                     << std::endl;
     kmeans::train_result result_train =
         preview::train(comm, kmeans_desc, local_input);
+    std::cout << "kmeans oneapi training 3"
+                      << std::endl;
     if (isRoot) {
         // Get the class of the input object
         jclass clazz = env->GetObjectClass(resultObj);
@@ -81,6 +296,8 @@ static jlong doKMeansOneAPICompute(JNIEnv *env, jint rankId, jlong pNumTabData,
         return (jlong)0;
     }
 }
+#endif
+
 
 /*
  * Class:     com_intel_oap_mllib_clustering_KMeansDALImpl
@@ -92,14 +309,39 @@ Java_com_intel_oap_mllib_clustering_KMeansDALImpl_cKMeansOneapiComputeWithInitCe
     JNIEnv *env, jobject obj, jlong pNumTabData, jlong pNumTabCenters,
     jint clusterNum, jdouble tolerance, jint iterationNum, jint executorNum,
     jint computeDeviceOrdinal, jint rankId, jstring ipPort, jobject resultObj) {
-    std::cout << "oneDAL (native): use DPC++ kernels " << std::endl;
+    std::cout << "oneDAL (native): use DPC++ kernels "
+        << "; device = " << ComputeDeviceString[computeDeviceOrdinal]
+        << std::endl;
     const char *ipPortPtr = env->GetStringUTFChars(ipPort, 0);
     std::string ipPortStr = std::string(ipPortPtr);
     jlong ret = 0L;
-    ret = doKMeansOneAPICompute(
-        env, rankId, pNumTabData, pNumTabCenters, clusterNum, tolerance,
-        iterationNum, executorNum, ipPortStr, computeDeviceOrdinal, resultObj);
+    ComputeDevice device = getComputeDeviceByOrdinal(computeDeviceOrdinal);
+    switch (device) {
+#ifdef CPU_ONLY_PROFILE
+    case ComputeDevice::host:
+    case ComputeDevice::cpu: {
+         ccl::communicator &comm = getComm();
+         int rankId = comm.rank();
+         NumericTablePtr pData = *((NumericTablePtr *)pNumTabData);
+         NumericTablePtr centroids = *((NumericTablePtr *)pNumTabCenters);
+         ret = doKMeansDaalCompute(env, obj, rankId, comm,
+                                   pData, centroids, clusterNum,
+                                   tolerance, iterationNum, executorNum,
+                                   resultObj);
+    }
+#else
+    case ComputeDevice::gpu: {
+        ret = doKMeansOneAPICompute(
+                env, rankId, pNumTabData, pNumTabCenters, clusterNum, tolerance,
+                iterationNum, executorNum, ipPortStr, device, resultObj);
+    }
+#endif
+    default: {
+        std::cout << "No Device!" << std::endl;
+        exit(-1);
+    }
+    }
+
     env->ReleaseStringUTFChars(ipPort, ipPortPtr);
     return ret;
 }
-#endif
