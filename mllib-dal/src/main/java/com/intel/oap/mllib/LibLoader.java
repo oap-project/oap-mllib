f32c320f8d208d640d483c0c33b7043bfad424ac
minmingzhu <minming.zhu@intel.com>
2023-03-28 15:52:28 +0000

    remove load sycl and ccl lib
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/java/com/intel/oap/mllib/LibLoader.java
+++ mllib-dal/src/main/java/com/intel/oap/mllib/LibLoader.java
@@ -50,10 +50,10 @@ public final class LibLoader {
       return;
     }
 
-    if (!loadLibSYCL()) {
-      log.debug("SYCL libraries are not available, will load CPU libraries only.");
-    }
-    loadLibCCL();
+//    if (!loadLibSYCL()) {
+//      log.debug("SYCL libraries are not available, will load CPU libraries only.");
+//    }
+//    loadLibCCL();
     loadLibMLlibDAL();
 
     isLoaded = true;
