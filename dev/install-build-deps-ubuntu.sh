28f6cbd95ad8d32d95770e559b904c8d2242caf9
minmingzhu <minming.zhu@intel.com>
2023-03-31 12:45:42 +0000

    update

--- dev/install-build-deps-ubuntu.sh
+++ dev/install-build-deps-ubuntu.sh
@@ -7,8 +7,8 @@ if [ ! -d /opt/intel/oneapi ]; then
   | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
   # add signed entry to apt sources and configure the APT client to use Intel repository:
   echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
-  sudo apt update
-  sudo apt install -y intel-basekit
+  sudo apt-get update
+  sudo apt-get install -y intel-basekit
 else
   echo "oneAPI components already installed!"
 fi
