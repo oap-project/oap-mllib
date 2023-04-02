1d1a20eec3b5ddb694f426562e34c63bd06ad31e
minmingzhu <minming.zhu@intel.com>
2023-04-02 15:01:32 +0000

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/ci/ci-local-test.sh
+++ dev/ci/ci-local-test.sh
@@ -20,5 +20,5 @@ cd $GITHUB_WORKSPACE/mllib-dal
 ../dev/prepare-build-deps.sh
 ./build.sh -p CPU_ONLY_PROFILE -q
 
-unset LD_LIBRARY_PATH
+ldd src/main/resources/lib/libMLlibDAL.so
 ./test.sh -p CPU_ONLY_PROFILE -q
