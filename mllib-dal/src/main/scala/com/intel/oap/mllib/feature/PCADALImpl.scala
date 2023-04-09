b5f83171fc9a77e4d3fa21f535889dd778507c64
minmingzhu <minming.zhu@intel.com>
2023-04-09 15:19:29 +0000

    update PCADALImpl.scala
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/scala/com/intel/oap/mllib/feature/PCADALImpl.scala
+++ mllib-dal/src/main/scala/com/intel/oap/mllib/feature/PCADALImpl.scala
@@ -170,9 +170,9 @@ class PCADALImpl(val k: Int,
     new DenseMatrix(numCols, k, arrayDouble.slice(0, numCols * k), false)
   }
 
-  private[mllib] def getExplainedVarianceFromDAL(table_1xn: HomogenTable, k: Int,
-                                          device: Common.ComputeDevice): DenseVector = {
-    val arrayDouble = getDoubleBufferDataFromDAL(table_1xn, 1, device)
+  private[mllib] def getExplainedVarianceFromDAL(table_1xn: NumericTable, k: Int): DenseVector = {
+    val dataNumCols = table_1xn.getNumberOfColumns.toInt
+    val arrayDouble = getDoubleBufferDataFromDAL(table_1xn, 1, dataNumCols)
     val sum = arrayDouble.sum
     val topK = Arrays.copyOfRange(arrayDouble, 0, k)
     for (i <- 0 until k)
@@ -182,16 +182,19 @@ class PCADALImpl(val k: Int,
 
   // table.asInstanceOf[HomogenNumericTable].getDoubleArray() would error on GPU,
   // so use table.getBlockOfRows instead of it.
-  private[mllib] def getDoubleBufferDataFromDAL(table: HomogenTable,
+  private[mllib] def getDoubleBufferDataFromDAL(table: NumericTable,
                                          numRows: Int,
-                                         device: Common.ComputeDevice): Array[Double] = {
+                                         numCols: Int): Array[Double] = {
+    var dataDouble: DoubleBuffer = null
 
     // returned DoubleBuffer is ByteByffer, need to copy as double array
-    val accessor = new RowAccessor(table.getcObejct(), device)
-    val arrayDouble: Array[Double] = accessor.pullDouble(0, numRows)
+    dataDouble = table.getBlockOfRows(0, numRows, dataDouble)
+    val arrayDouble: Array[Double] = new Array[Double](numRows * numCols)
+    dataDouble.get(arrayDouble)
 
     arrayDouble
   }
+
 
 
   // Single entry to call Correlation PCA DAL backend with parameter K
