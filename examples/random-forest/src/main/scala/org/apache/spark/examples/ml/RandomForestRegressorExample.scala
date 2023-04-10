1474093f1a517d7a69a73878374d0609b9550828
minmingzhu <minming.zhu@intel.com>
2023-04-10 13:35:16 +0000

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- examples/random-forest/src/main/scala/org/apache/spark/examples/ml/RandomForestRegressorExample.scala
+++ examples/random-forest/src/main/scala/org/apache/spark/examples/ml/RandomForestRegressorExample.scala
@@ -22,8 +22,11 @@ package org.apache.spark.examples.ml
 // $example on$
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.ml.feature.{LabeledPoint, VectorIndexer}
 import org.apache.spark.ml.regression.{RandomForestRegressionModel, RandomForestRegressor}
+import org.apache.spark.sql.Row
+import org.apache.spark.ml.linalg.{DenseVector, Vector, Vectors}
+
 // $example off$
 import org.apache.spark.sql.SparkSession
 
@@ -43,13 +46,17 @@ object RandomForestRegressorExample {
       .select("label","features").rdd.map {
       case Row(label: Double ,feature: Vector) => new LabeledPoint(label, feature.toDense)
     }
+
+    import spark.implicits._
+    val df = featuresRDD.toDF("label", "features")
+    df.show()
     // Automatically identify categorical features, and index them.
     // Set maxCategories so features with > 4 distinct values are treated as continuous.
     val featureIndexer = new VectorIndexer()
       .setInputCol("features")
       .setOutputCol("indexedFeatures")
       .setMaxCategories(4)
-      .fit(data)
+      .fit(df)
 
     // Split the data into training and test sets (30% held out for testing).
     val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
