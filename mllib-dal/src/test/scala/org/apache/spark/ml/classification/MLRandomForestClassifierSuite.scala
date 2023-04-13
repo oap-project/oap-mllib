a52a95c2fc5864217b36fc81e30c148281cf9fb5
minmingzhu <minming.zhu@intel.com>
2023-04-13 08:20:21 +0000

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/test/scala/org/apache/spark/ml/classification/MLRandomForestClassifierSuite.scala
+++ mllib-dal/src/test/scala/org/apache/spark/ml/classification/MLRandomForestClassifierSuite.scala
@@ -65,17 +65,17 @@ class RandomForestClassifierSuite extends MLTest with DefaultReadWriteTest {
   // Tests calling train()
   /////////////////////////////////////////////////////////////////////////////
 
-  def binaryClassificationTestWithContinuousFeatures(rf: RandomForestClassifier): Unit = {
-    val categoricalFeatures = Map.empty[Int, Int]
-    val numClasses = 2
-    val newRF = rf
-      .setImpurity("Gini")
-      .setMaxDepth(2)
-      .setNumTrees(1)
-      .setFeatureSubsetStrategy("auto")
-      .setSeed(123)
-    compareAPIs(orderedLabeledPoints50_1000, newRF, categoricalFeatures, numClasses)
-  }
+//  def binaryClassificationTestWithContinuousFeatures(rf: RandomForestClassifier): Unit = {
+//    val categoricalFeatures = Map.empty[Int, Int]
+//    val numClasses = 2
+//    val newRF = rf
+//      .setImpurity("Gini")
+//      .setMaxDepth(2)
+//      .setNumTrees(1)
+//      .setFeatureSubsetStrategy("auto")
+//      .setSeed(123)
+//    compareAPIs(orderedLabeledPoints50_1000, newRF, categoricalFeatures, numClasses)
+//  }
 
   test("params") {
     ParamsSuite.checkParams(new RandomForestClassifier)
@@ -84,58 +84,58 @@ class RandomForestClassifierSuite extends MLTest with DefaultReadWriteTest {
     ParamsSuite.checkParams(model)
   }
 
-  test("Binary classification with continuous features:" +
-    " comparing DecisionTree vs. RandomForest(numTrees = 1)") {
-    val rf = new RandomForestClassifier()
-      .setBootstrap(false)
-    binaryClassificationTestWithContinuousFeatures(rf)
-  }
+//  test("Binary classification with continuous features:" +
+//    " comparing DecisionTree vs. RandomForest(numTrees = 1)") {
+//    val rf = new RandomForestClassifier()
+//      .setBootstrap(false)
+//    binaryClassificationTestWithContinuousFeatures(rf)
+//  }
 
-  test("Binary classification with continuous features and node Id cache:" +
-    " comparing DecisionTree vs. RandomForest(numTrees = 1)") {
-    val rf = new RandomForestClassifier()
-      .setBootstrap(false)
-      .setCacheNodeIds(true)
-    binaryClassificationTestWithContinuousFeatures(rf)
-  }
+//  test("Binary classification with continuous features and node Id cache:" +
+//    " comparing DecisionTree vs. RandomForest(numTrees = 1)") {
+//    val rf = new RandomForestClassifier()
+//      .setBootstrap(false)
+//      .setCacheNodeIds(true)
+//    binaryClassificationTestWithContinuousFeatures(rf)
+//  }
 
-  test("alternating categorical and continuous features with multiclass labels to test indexing") {
-    val arr = Array(
-      LabeledPoint(0.0, Vectors.dense(1.0, 0.0, 0.0, 3.0, 1.0)),
-      LabeledPoint(1.0, Vectors.dense(0.0, 1.0, 1.0, 1.0, 2.0)),
-      LabeledPoint(0.0, Vectors.dense(2.0, 0.0, 0.0, 6.0, 3.0)),
-      LabeledPoint(2.0, Vectors.dense(0.0, 2.0, 1.0, 3.0, 2.0))
-    )
-    val rdd = sc.parallelize(arr)
-    val categoricalFeatures = Map(0 -> 3, 2 -> 2, 4 -> 4)
-    val numClasses = 3
+//  test("alternating categorical and continuous features with multiclass labels to test indexing") {
+//    val arr = Array(
+//      LabeledPoint(0.0, Vectors.dense(1.0, 0.0, 0.0, 3.0, 1.0)),
+//      LabeledPoint(1.0, Vectors.dense(0.0, 1.0, 1.0, 1.0, 2.0)),
+//      LabeledPoint(0.0, Vectors.dense(2.0, 0.0, 0.0, 6.0, 3.0)),
+//      LabeledPoint(2.0, Vectors.dense(0.0, 2.0, 1.0, 3.0, 2.0))
+//    )
+//    val rdd = sc.parallelize(arr)
+//    val categoricalFeatures = Map(0 -> 3, 2 -> 2, 4 -> 4)
+//    val numClasses = 3
+//
+//    val rf = new RandomForestClassifier()
+//      .setImpurity("Gini")
+//      .setMaxDepth(5)
+//      .setNumTrees(2)
+//      .setFeatureSubsetStrategy("sqrt")
+//      .setSeed(12345)
+//    compareAPIs(rdd, rf, categoricalFeatures, numClasses)
+//  }
 
-    val rf = new RandomForestClassifier()
-      .setImpurity("Gini")
-      .setMaxDepth(5)
-      .setNumTrees(2)
-      .setFeatureSubsetStrategy("sqrt")
-      .setSeed(12345)
-    compareAPIs(rdd, rf, categoricalFeatures, numClasses)
-  }
-
-  test("subsampling rate in RandomForest") {
-    val rdd = orderedLabeledPoints5_20
-    val categoricalFeatures = Map.empty[Int, Int]
-    val numClasses = 2
-
-    val rf1 = new RandomForestClassifier()
-      .setImpurity("Gini")
-      .setMaxDepth(2)
-      .setCacheNodeIds(true)
-      .setNumTrees(3)
-      .setFeatureSubsetStrategy("auto")
-      .setSeed(123)
-    compareAPIs(rdd, rf1, categoricalFeatures, numClasses)
-
-    val rf2 = rf1.setSubsamplingRate(0.5)
-    compareAPIs(rdd, rf2, categoricalFeatures, numClasses)
-  }
+//  test("subsampling rate in RandomForest") {
+//    val rdd = orderedLabeledPoints5_20
+//    val categoricalFeatures = Map.empty[Int, Int]
+//    val numClasses = 2
+//
+//    val rf1 = new RandomForestClassifier()
+//      .setImpurity("Gini")
+//      .setMaxDepth(2)
+//      .setCacheNodeIds(true)
+//      .setNumTrees(3)
+//      .setFeatureSubsetStrategy("auto")
+//      .setSeed(123)
+//    compareAPIs(rdd, rf1, categoricalFeatures, numClasses)
+//
+//    val rf2 = rf1.setSubsamplingRate(0.5)
+//    compareAPIs(rdd, rf2, categoricalFeatures, numClasses)
+//  }
 
   test("predictRaw and predictProbability") {
     val rdd = orderedLabeledPoints5_20
