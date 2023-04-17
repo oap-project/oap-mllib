d8bca4365688dc192489321f66a3bfb77ad0cff1
minmingzhu <minming.zhu@intel.com>
2023-04-17 12:27:56 +0000

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/test/scala/com/intel/oap/mllib/TestCommon.scala
+++ mllib-dal/src/test/scala/com/intel/oap/mllib/TestCommon.scala
@@ -102,7 +102,7 @@ object TestCommon {
     var computeDevice: Common.ComputeDevice = Common.ComputeDevice.HOST
     if(device != null) {
       device.toUpperCase match {
-        case "HOST" => computeDevice = Common.ComputeDevice.GPU
+        case "HOST" => computeDevice = Common.ComputeDevice.HOST
         case "CPU" => computeDevice = Common.ComputeDevice.CPU
         case "GPU" => computeDevice = Common.ComputeDevice.GPU
         case _ => "Invalid Device"
