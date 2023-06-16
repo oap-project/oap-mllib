f5eeaa93bbd20b77664fa90ae2eb724d97dd0a40
kunpeng <kunpeng.jiang@intel.com>
2023-06-16 01:34:45 -0700

    Add tmp utils file

---
+++ examples/scala/change_name.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+exampleDirs=(kmeans pca als naive-bayes linear-regression correlation summarizer)
+
+for dir in ${exampleDirs[*]}
+do
+  cd $dir
+  echo
+  echo ===================
+  echo Building $dir ...
+  echo ===================
+  echo
+  mv ./run.sh ./run-cpu.sh
+  cd ..
+done
