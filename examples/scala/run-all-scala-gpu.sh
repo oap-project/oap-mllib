dda07a4e9f8d5c9e6d5a3be257d998370176c77a
kunpeng <kunpeng.jiang@intel.com>
2023-06-16 01:30:02 -0700

    Add run-all-gpu

---
+++ examples/scala/run-all-scala-gpu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+exampleDirs=(kmeans pca als naive-bayes linear-regression correlation summarizer)
+
+for dir in ${exampleDirs[*]}
+do
+  cd $dir
+  echo
+  echo ==========================
+  echo Running $dir ...
+  echo ==========================
+  echo
+  ./run.sh
+  cd ..
+done
