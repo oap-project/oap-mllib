70816ed38b5ce5dc3da4e210515ea0ac182b4a8e
kunpeng <kunpeng.jiang@intel.com>
2023-06-24 10:01:19 -0700

    Rename scala

--- examples/scala/change_name.sh
+++ examples/scala/change_name.sh
@@ -4,12 +4,10 @@ exampleDirs=(kmeans pca als naive-bayes linear-regression correlation summarizer
 
 for dir in ${exampleDirs[*]}
 do
-  cd $dir
+  mv $dir $dir-scala
   echo
   echo ===================
   echo Building $dir ...
   echo ===================
   echo
-  rm ./*.log
-  cd ..
 done
