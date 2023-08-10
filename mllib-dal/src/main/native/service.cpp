8e1d2aefe95871417dc1b4d824dddb404bae9827
kunpeng <kunpeng.jiang@intel.com>
2023-08-10 01:43:50 -0700

    Fix other printf, fix new line bug

--- mllib-dal/src/main/native/service.cpp
+++ mllib-dal/src/main/native/service.cpp
@@ -246,7 +246,7 @@ void printHomegenTable(const oneapi::dal::table &table) {
                 logger::print(logger::INFO, "%10f",
                               x[i * table.get_column_count() + j]);
             }
-            logger::println("");
+            logger::println(logger::NONE, "");
         }
     } else {
         for (std::int64_t i = 0; i < 5; i++) {
@@ -254,7 +254,7 @@ void printHomegenTable(const oneapi::dal::table &table) {
                 logger::print(logger::INFO, "%10f",
                               x[i * table.get_column_count() + j]);
             }
-            logger::println("");
+            logger::println(logger::NONE, "");
         }
         logger::println(logger::INFO, "...%ld lines skipped...",
                         (table.get_row_count() - 10));
@@ -264,7 +264,7 @@ void printHomegenTable(const oneapi::dal::table &table) {
                 logger::print(logger::INFO, "%10f",
                               x[i * table.get_column_count() + j]);
             }
-            logger::println("");
+            logger::println(logger::NONE, "");
         }
     }
     return 0;
