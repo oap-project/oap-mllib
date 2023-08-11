523d1b660dba50811a9c8066d5501e780fe7b879
kunpeng <kunpeng.jiang@intel.com>
2023-08-11 14:03:47 +0000

    Remove variable

--- examples/scala/linear-regression-scala/run-gpu.sh
+++ examples/scala/linear-regression-scala/run-gpu.sh
@@ -16,7 +16,6 @@ WORKER_GPU_AMOUNT=4
 EXECUTOR_GPU_AMOUNT=1
 TASK_GPU_AMOUNT=1
 
-export OAP_MLLIB_LOGGER_CPP_ENABLED=1
 # Should run in standalone mode
 time $SPARK_HOME/bin/spark-submit --master $SPARK_MASTER \
     --num-executors $SPARK_NUM_EXECUTORS \
