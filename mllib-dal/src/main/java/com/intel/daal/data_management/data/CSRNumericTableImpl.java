171a19344375f8f195e84636c1229f839b908357
minmingzhu <minming.zhu@intel.com>
2023-09-19 10:05:18 +0000

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/java/com/intel/daal/data_management/data/CSRNumericTableImpl.java
+++ mllib-dal/src/main/java/com/intel/daal/data_management/data/CSRNumericTableImpl.java
@@ -16,9 +16,7 @@
 /**
  * @ingroup numeric_tables @{
  */
-//checkstyle.off:PackageName
 package com.intel.daal.data_management.data;
-//checkstyle.on:PackageName
 
 import com.intel.daal.services.DaalContext;
 import com.intel.oap.mllib.LibLoader;
--- mllib-dal/src/main/java/com/intel/daal/data_management/data/DataDictionary.java
+++ mllib-dal/src/main/java/com/intel/daal/data_management/data/DataDictionary.java
@@ -19,7 +19,6 @@
  *        with the data dictionary
  * @ingroup data_management @{
  */
-@SuppressWarnings("checkstyle:PackageName")
 package com.intel.daal.data_management.data;
 
 import java.io.IOException;
