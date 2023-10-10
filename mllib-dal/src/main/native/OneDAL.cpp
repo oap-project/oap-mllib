8cba712a21bfd73c374763b33baadc6825ccb2d8
minmingzhu <minming.zhu@intel.com>
2023-10-10 03:47:58 +0000

    update code style
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- mllib-dal/src/main/native/OneDAL.cpp
+++ mllib-dal/src/main/native/OneDAL.cpp
@@ -228,6 +228,7 @@ Java_com_intel_oap_mllib_OneDAL_00024_cCopyDoubleArrayToFloatNative(
     jsize sourceLength = env->GetArrayLength(sourceArray);
     jdouble *source = static_cast<jdouble *>(
         env->GetPrimitiveArrayCritical(sourceArray, NULL));
-    std::transform(source, source + sourceLength, nativeArray + index, [](double d) { return static_cast<float>(d); });
+    std::transform(source, source + sourceLength, nativeArray + index,
+                   [](double d) { return static_cast<float>(d); });
     env->ReleasePrimitiveArrayCritical(sourceArray, source, 0);
 }
