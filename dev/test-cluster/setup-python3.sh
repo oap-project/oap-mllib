65b49a8908c38052e038bf40b553022484e6f197
minmingzhu <minming.zhu@intel.com>
2024-03-05 08:46:30 +0000

    make pip install a specific version of the package.
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- dev/test-cluster/setup-python3.sh
+++ dev/test-cluster/setup-python3.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -q
 sudo apt-get install -y -q python3-pip python3-setuptools python3-wheel
 
-pip3 install --user numpy -q
+pip3 install --user numpy==1.21.2 -q
 
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 
