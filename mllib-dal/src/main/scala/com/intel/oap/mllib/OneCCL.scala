18705ad0c7d8eff57c507e5411c8008187dc6b04
minmingzhu <minming.zhu@intel.com>
2024-08-15 15:03:38 +0800

    update

--- mllib-dal/src/main/scala/com/intel/oap/mllib/OneCCL.scala
+++ mllib-dal/src/main/scala/com/intel/oap/mllib/OneCCL.scala
@@ -28,11 +28,10 @@ object OneCCL extends Logging {
   def init(executor_num: Int, rank: Int, ip_port: String,
            computeDeviceOrdinal: Int = Common.ComputeDevice.CPU.ordinal()): Unit = {
 
-
     logInfo(s"Initializing with IP_PORT: ${ip_port}")
 
     // cclParam is output from native code
-    c_init(executor_num, rank, ip_port, computeDeviceOrdinal, cclParam)
+    c_init(executor_num, rank, ip_port, cclParam)
 
     // executor number should equal to oneCCL world size
     assert(executor_num == cclParam.getCommSize,
@@ -64,8 +63,7 @@ object OneCCL extends Logging {
 
   @native def c_getAvailPort(localIP: String): Int
 
-  @native private def c_init(size: Int, rank: Int, ip_port: String,
-                             computeDeviceOrdinal: Int, param: CCLParam): Int
+  @native private def c_init(size: Int, rank: Int, ip_port: String, param: CCLParam): Int
 
   @native private def c_cleanup(): Unit
 }
