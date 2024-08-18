8b5d91de059aecc69e6feba5effea57479f05fbc
minmingzhu <minming.zhu@intel.com>
2024-08-18 09:52:43 +0800

    optimize code

--- mllib-dal/src/main/native/GPU.h
+++ mllib-dal/src/main/native/GPU.h
@@ -6,7 +6,9 @@
 #include <daal_sycl.h>
 #include <jni.h>
 #include <oneapi/ccl.hpp>
+#include "Communicator.hpp"
 
 sycl::queue getAssignedGPU(const ComputeDevice device, jint *gpu_indices);
 
 sycl::queue getQueue(const ComputeDevice device);
+preview::spmd::communicator<preview::spmd::device_memory_access::usm> createDalCommunicator(jint executorNum, jint rank, ccl::string ccl_ip_port);
