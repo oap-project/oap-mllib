9d79b62bfcab483d3c6be0a400eb91081f764127
minmingzhu <minming.zhu@intel.com>
2025-04-07 11:34:36 +0800

    update

--- mllib-dal/src/main/native/SummarizerImpl.cpp
+++ mllib-dal/src/main/native/SummarizerImpl.cpp
@@ -224,7 +224,8 @@ static void doSummarizerOneAPICompute(
         auto t2 = std::chrono::high_resolution_clock::now();
         float duration = std::chrono::duration<float>(t2 - t1).count();
         logger::println(logger::INFO,
-                        "Summarizer (native): training step took %f secs", duration);
+                        "Summarizer (native): training step took %f secs",
+                        duration);
         // Return all covariance & mean
         jclass clazz = env->GetObjectClass(resultObj);
 
